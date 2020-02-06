d224e917a59d3c4d2f0c55834eebd1f44d130894
amansharma-dev <amansharma.11@hotmail.com>
2020-02-06 14:59:50 +0530

    new commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     //testing..
-//    $('h1').click(function(){
-//       
-//        $(this).css('background-color', '#3DDC84');
-//        
-//    });
-    
-        var countTopOffset =$(".section-01-countup").offset().top;
-        var countUpFinished = false;
-    
-    $(window).scroll(function(){
-       
-        if(!countUpFinished && window.pageYOffset > countTopOffset - $(window).height() +200 ){
-            
-            $('.counter').each(function(){
-               var element = $(this);
+    //    $('h1').click(function(){
+    //       
+    //        $(this).css('background-color', '#3DDC84');
+    //        
+    //    });
+
+    var countTopOffset = $(".section-01-countup").offset().top;
+    var countUpFinished = false;
+
+    $(window).scroll(function () {
+
+        if (!countUpFinished && window.pageYOffset > countTopOffset - $(window).height() + 250) {
+
+            $('.counter').each(function () {
+                var element = $(this);
                 var endVal = parseInt(element.text());
                 element.countup(endVal);
             })
-            
+
             countUpFinished = true;
         }
-        
+
     });
-    
-    
-    
+
+
+
 });
